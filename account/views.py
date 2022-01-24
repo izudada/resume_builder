868b4b28d281a6231fd6552ee3b28931c9ec8969
Anthony <tonyudeagbala@gmail.com>
2022-01-24 10:08:26 +0000

    Create register view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import  reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .models import User
+from .forms import RegisterForm
+
+
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            return render(response, 'registration/register.html', {'form' : form})
+    else:
+        form = RegisterForm()
+
+        return render(response, 'registration/register.html', {'form' : form})
+
+    return HttpResponseRedirect("/login/")
