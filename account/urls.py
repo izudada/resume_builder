6797c69c131292ce99f4c56df04bae4d7bf90cce
Anthony <tonyudeagbala@gmail.com>
2022-01-24 10:08:02 +0000

    Add register view

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import include, path
+from .views import register
 
 urlpatterns = [
+    path('register/', register, name = "register"),
     path('', include('django.contrib.auth.urls')),
 ]
