ac6989f0ef8fc2d28896f3d72b0c2d1aa2ede6b8
Anthony <tonyudeagbala@gmail.com>
2022-01-28 23:19:16 +0000

    Edit gitignore

--- resume/models.py
+++ resume/models.py
@@ -3,6 +3,9 @@ from django.db import models
 from account.models import User, TrackingModel
 
 
-class AboutMe(TrackingModel, models.Model):
+class Profile(TrackingModel, models.Model):
+    """
+        Model for singular resume details
+    """
     about_me = models.TextField()
     belongs_to = models.ForeignKey(User, related_name="belongs_to", on_delete= models.CASCADE, null=False)
