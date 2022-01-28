ee571a3af6bedcbafdb91ec0b0f7fc18a13e0b6b
Anthony <tonyudeagbala@gmail.com>
2022-01-28 07:47:22 +0000

    Add resume.urls

--- app/settings.py
+++ app/settings.py
@@ -136,5 +136,6 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "login"
