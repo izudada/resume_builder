b3ec008af2c682f10685aab7fe71bca8790bab50
Anthony <tonyudeagbala@gmail.com>
2022-01-28 23:16:46 +0000

    Create urls file

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User, TrackingModel
+
+
+class AboutMe(TrackingModel, models.Model):
+    about_me = models.TextField()
+    belongs_to = models.ForeignKey(User, related_name="belongs_to", on_delete= models.CASCADE, null=False)
