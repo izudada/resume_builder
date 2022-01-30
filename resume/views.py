9ba864e9f514c80492359cfe3b9f2e8ba4c940cb
Anthony <tonyudeagbala@gmail.com>
2022-01-30 08:59:44 +0000

    Create UserProfile view

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.urls import  reverse_lazy, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import DetailView, TemplateView
 
-# Create your views here.
+from .models import Profile
+
+
+class UserProfile(LoginRequiredMixin, TemplateView):
+    model = Profile
+    fields = '__all__'
+    template_name = 'resume/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = Profile.objects.all()
+        return context
